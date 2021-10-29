214b430db098850d3383cac76f32e67c1ac6ce99
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-29 13:25:01 -0700

    refactored a couple projects endpoints

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -31,12 +31,8 @@ router.get("/:id", validateProjectId, async (req, res, next) => {
 });
 router.post("/", validateNewProject, async (req, res, next) => {
   try {
-    const { name, description, completed = false } = req.body;
-    const newProject = await Project.insert({
-      name,
-      description,
-      completed,
-    });
+    const { completed = false } = req.body;
+    const newProject = await Project.insert({ ...req.body, completed });
     res.status(201).json(newProject);
   } catch (err) {
     next(err);
@@ -49,12 +45,7 @@ router.put(
   async (req, res, next) => {
     try {
       const { id } = req.params;
-      const { name, description, completed } = req.body;
-      const updatedProject = await Project.update(id, {
-        name,
-        description,
-        completed,
-      });
+      const updatedProject = await Project.update(id, req.body);
       res.status(200).json(updatedProject);
     } catch (err) {
       next(err);
@@ -65,7 +56,7 @@ router.delete("/:id", validateProjectId, async (req, res, next) => {
   try {
     const { id } = req.params;
     await Project.remove(id);
-    res.end(); // shoots back a response with no payload
+    res.end();
   } catch (err) {
     next(err);
   }
